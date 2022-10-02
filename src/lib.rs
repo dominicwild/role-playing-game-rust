593e8346be06f9a5a91c9768e427e84b8b8e7eb7
dominicwild <dominicwild@dominicwild.com>
2022-10-02 23:11:29 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::ptr::null;
+
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        match self.health {
+            h if h <= 0 => Some(self.revived_player()),
+            _ => None
+        }
+    }
+
+    fn revived_player(&self) -> Player {
+        Player {
+            health: 100,
+            mana: self.derive_mana(),
+            level: self.level
+        }
+    }
+
+    fn derive_mana(&self) -> Option<u32> {
+        let mut mana = None;
+        if self.level >= 10 {
+            mana = Some(100);
+        }
+        mana
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        if self.mana.is_none() {
+            self.decrease_health(mana_cost);
+            return 0;
+        }
+
+        if self.mana.unwrap() < mana_cost {
+            return 0;
+        }
+
+        self.mana = self.mana.map(|mana| mana - mana_cost);
+
+        let damage = mana_cost*2;
+        damage
+    }
+
+    fn decrease_health(&mut self, damage: u32) {
+        self.health = self.health.saturating_sub(damage)
+    }
+}
